ca2176955a17f93ef593be5e42722aa63164e245
avishkar <avishkar.raut@axonichealth.com>
2023-10-23 12:05:14 +0530

    Src

---
+++ src/main/java/com/example/GoBankers/Repo/UserRepo.java
@@ -0,0 +1,21 @@
+package com.example.GoBankers.Repo;
+
+import com.example.GoBankers.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@EnableJpaRepositories
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+
+    @Query(value = "select u from Users u")
+    public List<Users> getUsers();
+
+
+//    @Query(value = "select u from Users u where name=:val")
+//    public List<Users> getUsers(@Param("val") String firstName);
+}
